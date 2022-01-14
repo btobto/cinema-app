4272de51d8bc1a5cd7272548602f00b1802d98ee
btobto <btosic16@gmail.com>
2022-01-14 01:03:07 +0100

    registration and login interface

--- Client/scripts/views/cinema.view.js
+++ Client/scripts/views/cinema.view.js
@@ -1,54 +1,111 @@
 export class CinemaView {
 	constructor(cinema) {
 		this.cinema = cinema;
+		this.container = null;
 	}
 
-	draw(host) {
+	async draw(host) {
 		const container = document.createElement("div");
 		container.className = "cinemaContainer";
+		this.container = container;
 
-		const header = document.createElement("div");
-		header.className = "cinemaHeader";
+		// header
+
+		const cinemaHeader = document.createElement("div");
+		cinemaHeader.className = "cinemaHeader";
 
 		const cinemaInfo = document.createElement("div");
 		cinemaInfo.className = "headerBox";
-		cinemaInfo.style.flexGrow = 4;
-		const title = document.createElement("h1");
-		title.innerHTML = this.cinema.name;
-		const info = document.createElement("h2");
-		info.innerHTML = `${this.cinema.address}, ${this.cinema.city}`;
-		cinemaInfo.appendChild(title);
-		cinemaInfo.appendChild(info);
+		cinemaInfo.style.flexGrow = 15;
 
-		const userInfo = document.createElement("div");
-		userInfo.className = "headerBox";
-		userInfo.style.flexGrow = 1;
-		const login = document.createElement("h3");
-		login.innerHTML = "Log in";
-		const register = document.createElement("h3");
-		register.innerHTML = "Register";
-		userInfo.appendChild(login);
-		userInfo.appendChild(register);
+		// name
 
-		header.appendChild(cinemaInfo);
-		header.appendChild(userInfo);
-		container.appendChild(header);
+		const cinemaTitle = document.createElement("h1");
+		cinemaTitle.innerHTML = this.cinema.name;
+		cinemaTitle.className = "cinemaInfo";
+		cinemaTitle.addEventListener("click", event => {
+			this.drawCurrentMovies();
+		});
+
+		// address
+
+		const cinemaAddress = document.createElement("h2");
+		cinemaAddress.innerHTML = `${this.cinema.address}, ${this.cinema.city}`;
+		cinemaAddress.className = "cinemaInfo";
+
+		cinemaInfo.appendChild(cinemaTitle);
+		cinemaInfo.appendChild(cinemaAddress);
+
+		// user
+
+		const cinemaUser = document.createElement("div");
+		cinemaUser.className = "headerBox";
+		
+		const userContainer = document.createElement("div");
+		userContainer.className = "userContainer";
+
+		// login button
+
+		const loginOutline = document.createElement("div");
+		loginOutline.classList.add("outline", "login");
+		loginOutline.addEventListener("click", event => {
+			this.drawLoginMenu();
+		});
+		const loginHeader = document.createElement("h3");
+		loginHeader.innerHTML = "Log in";
+
+		loginOutline.appendChild(loginHeader)
+
+		// register button
+
+		const registerOutline = document.createElement("div");
+		registerOutline.classList.add("outline", "register")
+		registerOutline.addEventListener("click", event => {
+			this.drawRegisterMenu();
+		});
+		const registerHeader = document.createElement("h3");
+		registerHeader.innerHTML = "Register";
+
+		registerOutline.appendChild(registerHeader)
+
+		userContainer.appendChild(loginOutline);
+		userContainer.appendChild(registerOutline);
+		cinemaUser.appendChild(userContainer);
+
+		cinemaHeader.appendChild(cinemaInfo);
+		cinemaHeader.appendChild(cinemaUser);
+
+		// available movies
 
 		const content = document.createElement("div");
-		content.className = "cinemaContent";
+		content.classList.add("cinemaContent");
+		
+		container.appendChild(cinemaHeader);
+		container.appendChild(content);
 
-		console.log(this.cinema.movies);
-		console.log("length: " + this.cinema.movies.length);
+		this.drawCurrentMovies();
 
-		if (this.cinema.movies.length > 0)
-		{
+		host.appendChild(container);
+	}
+
+	async drawCurrentMovies() {
+		const host = this.container.querySelector(".cinemaContent");
+		host.innerHTML = "";
+		host.classList.remove("showMenu");
+		host.classList.add("showMovies");
+
+		if (this.cinema.movies.length > 0) {
 			for (const movie of this.cinema.movies) {
 				const movieContainer = document.createElement("div");
 				movieContainer.className = "movieContainer";
 
+				// hover text
+
 				const titleContaier = document.createElement("div");
 				titleContaier.className = "titleContainer";
 				titleContaier.innerHTML = `${movie.name} (${movie.year})`;
+
+				// poster
 
 				const posterContainer = document.createElement("div");
 				posterContainer.className = "posterContainer";
@@ -68,18 +125,126 @@ export class CinemaView {
 
 				movieContainer.appendChild(titleContaier);
 				movieContainer.appendChild(posterContainer);
-				content.appendChild(movieContainer);
+				host.appendChild(movieContainer);
 			}
-		}
-		else {
+		} else {
 			const moviesInfo = document.createElement("h2");
 			moviesInfo.innerHTML = "No screenings available.";
-			content.appendChild(moviesInfo);
+			host.appendChild(moviesInfo);
 		}
-
-		container.appendChild(content);
-		host.appendChild(container);
 	}
 
+	async drawLoginMenu() {
+		const host = this.container.querySelector(".cinemaContent");
+		host.innerHTML = "";
+		host.classList.remove("showMovies");
+		host.classList.add("showMenu");
+		
+		// title
+
+		let div = document.createElement("div");
+		div.className = "inputDiv";
+		const title = document.createElement("h1");
+		title.style.marginTop = 0;
+		title.innerHTML = "Log in";
+
+		const line = document.createElement("hr");
+
+		div.appendChild(title);
+		div.appendChild(line);
+		host.appendChild(div);
+		
+		// form
+
+		const form = document.createElement("form");
+
+		this.drawInput(form, "email", "E-mail:");
+		this.drawInput(form, "password", "Password:");
+
+		// submit button
+
+		div = document.createElement("div");
+		const submitBtn = document.createElement("button");
+		submitBtn.className = "outline";
+		const text = document.createElement("h3");
+		text.innerHTML = "Log in";
+		submitBtn.appendChild(text);
+
+		div.style.marginBottom = "20px";
+		div.appendChild(submitBtn);
+		form.appendChild(div);
 	
+		host.appendChild(form);
+	}
+
+	async drawRegisterMenu() {
+		const host = this.container.querySelector(".cinemaContent");
+		host.innerHTML = "";
+		host.classList.remove("showMovies");
+		host.classList.add("showMenu");
+		
+		// title
+
+		let div = document.createElement("div");
+		div.className = "inputDiv";
+		const title = document.createElement("h1");
+		title.style.marginTop = 0;
+		title.innerHTML = "Register";
+
+		const line = document.createElement("hr");
+
+		div.appendChild(title);
+		div.appendChild(line);
+		host.appendChild(div);
+		
+		// form
+
+		const form = document.createElement("form");
+
+		this.drawInput(form, "text", "First name:");
+		this.drawInput(form, "text", "Last name:");
+		this.drawInput(form, "email", "E-mail:");
+		this.drawInput(form, "password", "Password:");
+		this.drawInput(form, "tel", "Phone number:");
+
+		// submit button
+
+		div = document.createElement("div");
+		const submitBtn = document.createElement("button");
+		submitBtn.className = "outline";
+		const text = document.createElement("h3");
+		text.innerHTML = "Register";
+		submitBtn.appendChild(text);
+
+		div.style.marginBottom = "20px";
+		div.appendChild(submitBtn);
+		form.appendChild(div);
+	
+		host.appendChild(form);
+
+	}
+
+	async drawInput(form, inputType, labelText) {
+		let div = document.createElement("div");
+
+		// label div
+
+		let container = document.createElement("div");
+		let label = document.createElement("label");
+		label.innerHTML = labelText;
+		container.appendChild(label);
+		div.appendChild(container);
+
+		// input div
+
+		container = document.createElement("div");
+		let input = document.createElement("input");
+		input.type = inputType;
+		input.required = true;
+		container.appendChild(input);
+		div.style.marginBottom = "15px";
+		div.appendChild(container);
+
+		form.appendChild(div);
+	}
 }
